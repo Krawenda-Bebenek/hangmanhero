896f31af6e6214820ae35494e271ad4f72f5ef2d
Ewa Krawenda-Bebenek <ewa.krawenda-bebenek@openstax.pl>
2021-11-10 09:09:27 +0100

    initial commit

---
+++ src/main/java/com/ekb/hangmanhero/HangmanHeroApplication.java
@@ -0,0 +1,13 @@
+package com.ekb.hangmanhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekb/hangmanhero/HangmanHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekb.hangmanhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
